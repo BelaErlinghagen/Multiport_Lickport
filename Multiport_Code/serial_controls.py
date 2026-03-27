f333b403fa89352e339116627fbd212c73c61950
BelaErlinghagen <bela-erl@proton.me>
2026-03-27 11:57:54 +0100

    serial communication established

--- Multiport_Code/serial_controls.py
+++ Multiport_Code/serial_controls.py
@@ -1,7 +1,18 @@
 import serial
+import time
 
-def initialize_serial():
-    pass
+
+def initialize_serial_connections():
+    try:
+        time.sleep(2)  # Give time for Arduinos to reset
+        ser1 = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+        ser2 = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
+
+        print("[INFO] Serial connections initialized.")
+    except serial.SerialException as e:
+        print(f"[ERROR] Serial connection failed: {e}")
+        ser1 = None
+        ser2 = None
 
 def read_serial():
     pass
@@ -13,10 +24,10 @@ def pump_trigger(id, on_off, time):
     pass
 
 if __name__ == "__main__":
-    initialize_serial()
+    initialize_serial_connections()
 
     while True:
         output = read_serial()
         print(output)
         command = input(">>> ")
-        if command
+        print(command)
