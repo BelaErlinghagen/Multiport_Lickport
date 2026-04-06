7e2721ea75ff80406c9d81ec5f2b211807043083
BelaErlinghagen <bela-erl@proton.me>
2026-04-06 12:13:42 +0200

    Introduced downsampling and reshaping functionality

--- Multiport_Code/camera_controls.py
+++ Multiport_Code/camera_controls.py
@@ -1,14 +1,16 @@
 import time
 from pypylon import pylon
-import numpy as np
+from shared_states import IMG_HEIGHT, IMG_WIDTH
 
 class TrackingCamera:
-    def __init__(self, shared_image):
+    def __init__(self, shared_image, dlc_queue, plotting_shape):
         print("Initializing Camera.")
         self.camera = pylon.InstantCamera(
             pylon.TlFactory.GetInstance().CreateFirstDevice()
         )
         self.shared_image = shared_image
+        self.dlc_input = dlc_queue
+        self.shape = plotting_shape
         self.camera_on = False
         time.sleep(2)
         print("Camera is ready.")
@@ -16,14 +18,27 @@ class TrackingCamera:
     def run(self):
         self.camera.Open()
         self.camera_on = True
-        numberOfImagesToGrab = 100
-        self.camera.StartGrabbingMax(numberOfImagesToGrab)
+        self.camera.Gain.SetValue(48.0)
+        self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 
         while self.camera.IsGrabbing() and self.camera_on:
             grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
             if grabResult.GrabSucceeded():
-                img = grabResult.Array.astype(np.float32).flatten()
-                self.shared_image[:] = img
+                img = grabResult.Array
+                start_x = (IMG_WIDTH - IMG_HEIGHT) // 2
+                end_x = start_x + IMG_HEIGHT
+                cropped_img = img[:, start_x:end_x]
+                # send full quality cropped image to dlc
+                if self.dlc_input.full():
+                    self.dlc_input.get()
+                self.dlc_input.put(cropped_img)
+                # downsample image for camera plotting
+                target_h, target_w = self.shape
+                step_h = IMG_HEIGHT // target_h
+                step_w = IMG_HEIGHT // target_w
+                downsampled = cropped_img[::step_h, ::step_w][:target_h, :target_w]
+                print(downsampled)
+                self.shared_image[:] = downsampled.flatten()
             grabResult.Release()
 
         self.camera_on = False
@@ -31,3 +46,13 @@ class TrackingCamera:
 
     def stop(self):
         self.camera_on = False
+
+if __name__ == "__main__":
+    from multiprocessing import Queue, Array
+    import numpy as np
+    cam_shape = (400, 400)
+    cam_size = int(np.prod(cam_shape))
+    dlc_queue = Queue()
+    shared_image = Array('f', cam_size) 
+    camera = TrackingCamera(shared_image, dlc_queue, cam_shape)
+    camera.run()
--- Multiport_Code/main.py
+++ Multiport_Code/main.py
@@ -1,24 +1,24 @@
 # main.py
-from multiprocessing import Process, Array
+from multiprocessing import Process, Array, Queue
 import numpy as np
 from camera_controls import TrackingCamera
 from plotting_functions import run_gui
 from serial_controls import sensor_process
-from shared_states import IMG_HEIGHT, IMG_WIDTH
 
 
 def main():
-    cam_shape = (IMG_HEIGHT, IMG_WIDTH)
+    cam_shape = (400, 400)
     cam_size = int(np.prod(cam_shape))
+    dlc_queue = Queue()
     shared_image = Array('f', cam_size)        # shared camera
     sensor_array = Array('i', 16)             # shared sensors
 
     # Start camera
-    camera = TrackingCamera(shared_image)
+    camera = TrackingCamera(shared_image, dlc_queue, cam_shape)
     cam_proc = Process(target=camera.run)
     cam_proc.start()
 
-    # Start sensor simulation
+    # Start sensor grabbing
     sensor_proc = Process(target=sensor_process, args=(sensor_array,))
     sensor_proc.start()
 
